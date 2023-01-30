5c1bf97f1570d1be8856fb2d7b8ae8301ec3f192
ricardog07 <71521082+ricardog07@users.noreply.github.com>
2023-01-30 18:53:04 -0500

    Se vinculan los dos videos.

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -20,7 +20,7 @@
       .col-lg-12(data-aos="fade-left")     
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)            
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/ZfXemnyC8Sk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)            
                
 
 </template>
